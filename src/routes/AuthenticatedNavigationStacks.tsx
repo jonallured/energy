4d2b339bab3a845be707b3098a84374c2c8030fe
Sam Raj <rajsam003@gmail.com>
2022-05-04 12:18:13 +0200

    3 tabs

--- src/routes/AuthenticatedNavigationStacks.tsx
+++ src/routes/AuthenticatedNavigationStacks.tsx
@@ -3,6 +3,8 @@ import React from "react"
 import { GlobalStore } from "@store/GlobalStore"
 import { Tabs } from "react-native-collapsible-tab-view"
 import { StyleSheet, View, ListRenderItem } from "react-native"
+import { Flex, Text } from "palette"
+import { ArtistsScreen } from "@Scenes/Artists/Artists"
 
 const HEADER_HEIGHT = 250
 
@@ -10,7 +12,11 @@ const DATA = [0, 1, 2, 3, 4]
 const identity = (v: unknown): string => v + ""
 
 const Header = () => {
-  return <View style={styles.header} />
+  return (
+    <Flex px={2} pt={4}>
+      <Text variant="lg">Folio</Text>
+    </Flex>
+  )
 }
 
 const MainAuthenticatedStack: React.FC = () => {
@@ -23,14 +29,15 @@ const MainAuthenticatedStack: React.FC = () => {
       renderHeader={Header}
       headerHeight={HEADER_HEIGHT} // optional
     >
-      <Tabs.Tab name="A">
+      <Tabs.Tab name="Artists">
+        <Tabs.FlatList data={DATA} renderItem={renderItem} keyExtractor={identity} />
+        {/* <ArtistsScreen /> */}
+      </Tabs.Tab>
+      <Tabs.Tab name="Shows">
         <Tabs.FlatList data={DATA} renderItem={renderItem} keyExtractor={identity} />
       </Tabs.Tab>
-      <Tabs.Tab name="B">
-        <Tabs.ScrollView>
-          <View style={[styles.box, styles.boxA]} />
-          <View style={[styles.box, styles.boxB]} />
-        </Tabs.ScrollView>
+      <Tabs.Tab name="Albums">
+        <Tabs.FlatList data={DATA} renderItem={renderItem} keyExtractor={identity} />
       </Tabs.Tab>
     </Tabs.Container>
   )
