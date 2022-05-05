e221f59f83f36aa5f0fac794ddc77b326d11b774
Pavlos Vinieratos <pvinis@gmail.com>
2022-05-05 11:44:51 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Theme } from "palette"
+import { Tabs } from "react-native-collapsible-tab-view"
 import React, { ReactNode } from "react"
-import { LogBox } from "react-native"
+import { LogBox, Text, View } from "react-native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { GlobalStoreProvider } from "./store/GlobalStore"
 import { RelayEnvironmentProvider } from "react-relay/hooks"
@@ -9,6 +10,9 @@ import { defaultEnvironment } from "@relay/defaultEnvironent"
 import { MainNavigationStack } from "@routes/MainNavigationStack"
 import { HomeTabs } from "@Scenes/HomeTabs"
 import ScrollableTabBarExample from "@Scenes/HomeTabs-1"
+import { ShowsScreen } from "@Scenes/Shows/Shows"
+import { ArtistScreen } from "@Scenes/Artist/Artist"
+import { AlbumsScreen } from "@Scenes/Albums/Albums"
 LogBox.ignoreLogs(["Expected style "])
 
 const AppProviders = ({ children }: { children: ReactNode }) => (
@@ -22,6 +26,19 @@ const AppProviders = ({ children }: { children: ReactNode }) => (
 )
 
 export const App = () => {
+  return (
+    <View style={{ flex: 1, paddingTop: 60, backgroundColor: "orange" }}>
+      <Tabs.Container>
+        <Tabs.Tab name="Show">
+          <ShowsScreen />
+        </Tabs.Tab>
+        <Tabs.Tab name="Ablum">
+          <AlbumsScreen />
+        </Tabs.Tab>
+      </Tabs.Container>
+    </View>
+  )
+
   return (
     <AppProviders>
       <HomeTabs />
--- src/Scenes/Shows/Shows.tsx
+++ src/Scenes/Shows/Shows.tsx
@@ -1,7 +1,20 @@
-import { Flex, Text } from "palette"
+// import { Flex, Text } from "palette"
 import React from "react"
+import { Text, View } from "react-native"
+import { Tabs } from "react-native-collapsible-tab-view"
 
 export const ShowsScreen = () => {
+  return (
+    <Tabs.ScrollView>
+      <Text>Shows</Text>
+      <Text>wowoww</Text>
+      <Text>wowoww</Text>
+      <Text>wowoww</Text>
+      <Text>wowoww</Text>
+      <Text>wowoww</Text>
+      <Text>wowoiiiiiww</Text>
+    </Tabs.ScrollView>
+  )
   return (
     <Flex>
       <Text>Shows Screen</Text>
