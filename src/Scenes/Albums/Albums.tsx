96919146967f4c91ee5dbf9d574c97ec259712b0
Pavlos Vinieratos <pvinis@gmail.com>
2022-05-05 11:44:51 +0200

    wip

--- src/Scenes/Albums/Albums.tsx
+++ src/Scenes/Albums/Albums.tsx
@@ -1,7 +1,10 @@
-import { Flex, Text } from "palette"
+// import { Flex, Text } from "palette"
+import { View, Text } from "react-native"
 import React from "react"
+import { Tabs } from "react-native-collapsible-tab-view"
 
 export const AlbumsScreen = () => {
+  return <Tabs.FlatList data={[1, 2, 3, 4, 5]} renderItem={({ item }) => <Text>Album {item}</Text>} />
   return (
     <Flex>
       <Text>Albums Screen</Text>
