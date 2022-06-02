12f57fb5540692340701f16d0b1911a7bc15a7a5
Sam Raj <rajsam003@gmail.com>
2022-06-02 12:37:25 +0200

    fix artwork padding (#49)

--- src/Screens/HomeTabs/Artists/ArtistTabs/Artworks/Artworks.tsx
+++ src/Screens/HomeTabs/Artists/ArtistTabs/Artworks/Artworks.tsx
@@ -3,11 +3,9 @@ import { Flex, Text } from "palette"
 import { extractNodes } from "helpers/utils/extractNodes"
 import { TabsFlatList } from "helpers/components/TabsTestWrappers"
 import MasonryList from "@react-native-seoul/masonry-list"
-import { Dimensions, Image } from "react-native"
+import { Image } from "react-native"
 import { ArtworksQuery } from "__generated__/ArtworksQuery.graphql"
 import { Artworks_artwork$key } from "__generated__/Artworks_artwork.graphql"
-
-export const DEFAULT_SECTION_MARGIN = 20
 
 export const Artworks = ({ slug }: { slug: string }) => {
   const artworksData = useLazyLoadQuery<ArtworksQuery>(artworksQuery, { slug })
@@ -21,7 +19,7 @@ export const Artworks = ({ slug }: { slug: string }) => {
           testID="artist-artwork-list"
           contentContainerStyle={{
             marginTop: 20,
-            paddingLeft: 20,
+            paddingRight: 20,
           }}
           numColumns={2}
           data={artworks}
@@ -54,17 +52,15 @@ interface ArtworkThumbnailProps {
 
 export const ArtworkThumbnail: React.FC<ArtworkThumbnailProps> = (props) => {
   const artwork = useFragment<Artworks_artwork$key>(ArtworkThumbnailFragment, props.artwork)
-  const width = Dimensions.get("window").width
   return (
-    <Flex width={width / 2} mb={4}>
+    <Flex mb={4} pl={2}>
       <Image
         source={{ uri: Image.resolveAssetSource({ uri: artwork.image?.url! }).uri }}
         style={{
           aspectRatio: artwork.image?.aspectRatio ?? 1,
-          width: "80%",
         }}
       />
-      <Text italic variant="xs" color="black60" mt={1} mr={4}>
+      <Text italic variant="xs" color="black60" mt={1}>
         {artwork.title},{" "}
         <Text variant="xs" color="black60">
           {artwork.date}
