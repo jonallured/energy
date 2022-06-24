3123ac093c1e4cdada3cb5c7dbad73313e4e1a3b
Sam Raj <rajsam003@gmail.com>
2022-06-24 16:44:19 +0200

    refactor: Using useRoute() in ShowsTabs.tsx (#72)
    Using useRoute() in ShowsTabs.tsx

--- src/app/screens/HomeTabs/Shows/ShowTabs/ShowTabs.tsx
+++ src/app/screens/HomeTabs/Shows/ShowTabs/ShowTabs.tsx
@@ -1,23 +1,20 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { Flex, Text, Touchable, ArrowLeftIcon } from "palette"
-import { Suspense } from "react"
-import { ActivityIndicator } from "react-native"
 import { TabsContainer } from "app/wrappers/TabsContainer"
-import { TabBarProps, Tabs } from "react-native-collapsible-tab-view"
+import { Tabs } from "react-native-collapsible-tab-view"
 import { graphql, useLazyLoadQuery } from "react-relay"
 import { HomeTabsScreens } from "app/routes/HomeTabsNavigationStack"
 import { ShowTabsQuery } from "__generated__/ShowTabsQuery.graphql"
 import { ShowArtworks } from "./ShowArtworks/ShowArtworks"
 import { ShowInstalls } from "./ShowInstalls/ShowInstalls"
-
-type ShowProps = NativeStackScreenProps<HomeTabsScreens, "ShowTabs">
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
+import { SuspenseWrapper } from "app/wrappers/SuspenseWrapper"
 
 type HeaderProps = {
   showName: string
-  navigation: ShowProps["navigation"]
 }
 
-const Header: React.FC<TabBarProps & HeaderProps> = ({ showName, navigation }) => {
+const Header = ({ showName }: HeaderProps) => {
+  const navigation = useNavigation()
   return (
     <Flex px={2} mt={2}>
       <Touchable
@@ -34,28 +31,23 @@ const Header: React.FC<TabBarProps & HeaderProps> = ({ showName, navigation }) =
   )
 }
 
-export const ShowTabs: React.FC<ShowProps> = ({ route, navigation }) => {
-  const { slug } = route.params
+type ShowTabsRoute = RouteProp<HomeTabsScreens, "ArtistTabs">
+
+export const ShowTabs = () => {
+  const { slug } = useRoute<ShowTabsRoute>().params
 
   return (
-    <Suspense
-      fallback={
-        <Flex justifyContent={"center"} flex={1}>
-          <ActivityIndicator />
-        </Flex>
-      }
-    >
-      <RenderShow slug={slug} navigation={navigation} />
-    </Suspense>
+    <SuspenseWrapper>
+      <RenderShow slug={slug} />
+    </SuspenseWrapper>
   )
 }
 
 type RenderShowProps = {
   slug: string
-  navigation: ShowProps["navigation"]
 }
 
-const RenderShow: React.FC<RenderShowProps> = ({ slug, navigation }) => {
+const RenderShow: React.FC<RenderShowProps> = ({ slug }) => {
   const data = useLazyLoadQuery<ShowTabsQuery>(
     graphql`
       query ShowTabsQuery($slug: String!) {
@@ -68,9 +60,7 @@ const RenderShow: React.FC<RenderShowProps> = ({ slug, navigation }) => {
   )
 
   return (
-    <TabsContainer
-      header={(props) => <Header showName={data.show?.name!} navigation={navigation} {...props} />}
-    >
+    <TabsContainer header={(props) => <Header showName={data.show?.name!} {...props} />}>
       <Tabs.Tab name="ShowArtworks" label="Works">
         <ShowArtworks slug={slug} />
       </Tabs.Tab>
