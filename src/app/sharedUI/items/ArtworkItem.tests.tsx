cc25bfe8bdb2f03257197c1a2fb28a2fe5e12e56
Sam Raj <rajsam003@gmail.com>
2022-07-20 16:55:39 +0200

    fix: Album artworks test fix (#100)
    * Album artworks test fix
    
    * udpate
    
    * update

---
+++ src/app/sharedUI/items/ArtworkItem.tests.tsx
@@ -0,0 +1,32 @@
+import { mockEnvironmentPayloadMaybe } from "shared/tests/mockEnvironmentPayload"
+import { renderWithWrappers } from "shared/tests/renderWithWrappers"
+import { ArtworkItem } from "./ArtworkItem"
+
+describe("ArtworkItem", () => {
+  it("renders without throwing an error", async () => {
+    renderWithWrappers(<ArtworkItem artworkId={"some-id"} />)
+    await mockEnvironmentPayloadMaybe(mockProps)
+  })
+
+  it("renders the artwork item", async () => {
+    const { queryByText } = renderWithWrappers(<ArtworkItem artworkId={"some-id"} />)
+
+    await mockEnvironmentPayloadMaybe(mockProps)
+
+    expect(queryByText("artwork-1-name")).toBeDefined()
+  })
+})
+
+const mockProps = {
+  Artwork: () => ({
+    image: {
+      url: "some artwork url",
+      aspectRatio: 1,
+    },
+    name: "artwork-1-name",
+    price: "some price",
+    date: "some date",
+    id: "album-id",
+    internalId: "album-id",
+  }),
+}
