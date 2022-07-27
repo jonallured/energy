9480aa567565fb88bc720821809fbd06bf11b90e
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2022-07-27 07:37:41 -0400

    fix: add a placeholder test to fix ci (#115)
    add a placeholder test to fix ci

---
+++ ios/EnergyTests/EnergyTest.m
@@ -0,0 +1,23 @@
+#import <XCTest/XCTest.h>
+
+@interface EnergyTest : XCTestCase
+
+@end
+
+@implementation EnergyTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(YES);
+}
+
+@end
