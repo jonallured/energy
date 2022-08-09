6ce8fa3389117a7dce88df9ae05a4299c5313c82
Angela Patrinou <patrinoua@gmail.com>
2022-08-09 12:07:47 +0200

    feat(moplat-262): better centering (#131)

--- src/app/sharedUI/screens/InstallImage/InstallImage.tsx
+++ src/app/sharedUI/screens/InstallImage/InstallImage.tsx
@@ -1,5 +1,7 @@
 import { NavigationProp, RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import { ArrowLeftIcon, Flex, Touchable } from "palette"
+import { Header } from "app/sharedUI/Header"
+
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { HomeTabsScreens } from "app/navigation/HomeTabsNavigationStack"
 import { Image } from "react-native"
@@ -12,16 +14,9 @@ export const InstallImage = () => {
   const navigation = useNavigation<NavigationProp<HomeTabsScreens>>()
 
   return (
-    <Flex flex={1} pt={insets.top} mt={2}>
-      <Flex px={2}>
-        <Touchable
-          onPress={() =>  navigation.goBack()}
-        >
-          <ArrowLeftIcon fill="black100" />
-        </Touchable>
-      </Flex>
-
-      <Flex flex={1} justifyContent="center">
+    <Flex flex={1} mt={2}>
+      <Header />
+      <Flex flex={1} justifyContent="center" mb={5}>
         <Image source={{ uri: params.url }} style={{ aspectRatio: 1 }} />
       </Flex>
     </Flex>
