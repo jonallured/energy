b3fd7398f6d2d23cf7c1fb856d997df0e3a3af1b
Angela Patrinou <patrinoua@gmail.com>
2022-08-26 16:54:54 +0200

    feat(MOPLAT-397): Add progress bar to CMS web view (#149)
    * ProgressBar
    
    * address code review comments
    
    * remove safeAreaInsets
    
    * safeAreaInsets
    
    * remove timeout
    
    * remove absolute

--- src/app/sharedUI/screens/Artwork/ArtworkWebView.tsx
+++ src/app/sharedUI/screens/Artwork/ArtworkWebView.tsx
@@ -1,18 +1,30 @@
 import { RouteProp, useRoute } from "@react-navigation/native"
 import { HomeTabsScreens } from "app/navigation/HomeTabsNavigationStack"
 import { Header } from "app/sharedUI"
-import { Flex } from "palette"
+import { Flex, ProgressBar } from "palette"
+import { useState } from "react"
 import { WebView } from "react-native-webview"
 
 type ArtworkWebViewRoute = RouteProp<HomeTabsScreens, "ArtworkWebView">
 
 export const ArtworkWebView = () => {
   const { uri } = useRoute<ArtworkWebViewRoute>().params
+  const [loadProgress, setLoadProgress] = useState<number>(0)
+  const [isProgressBarVisible, setIsProgressBarVisible] = useState(true)
 
   return (
     <Flex flex={1} pb={2}>
       <Header safeAreaInsets />
-      <WebView source={{ uri }} />
+      <WebView
+        source={{ uri }}
+        onLoadProgress={(e) => setLoadProgress(e.nativeEvent.progress)}
+        onLoad={() => setIsProgressBarVisible(false)}
+      />
+      {isProgressBarVisible && (
+        <Flex px={4} height="100%" justifyContent="center">
+          <ProgressBar progress={loadProgress} />
+        </Flex>
+      )}
     </Flex>
   )
 }
