9070e0cee1ed0bba9a11c19159791c9bf9a0b4ae
Sam Raj <rajsam003@gmail.com>
2022-09-12 15:19:09 +0200

    fix: Album list image crop fix (#183)
    🙏

--- src/app/sharedUI/items/AlbumListItem.tsx
+++ src/app/sharedUI/items/AlbumListItem.tsx
@@ -1,29 +1,17 @@
 import { Album } from "app/store/Models/AlbumsModel"
-import { Flex, Text, useSpace } from "palette"
-import { useScreenDimensions } from "shared/hooks"
+import { Flex, Spacer, Text, useSpace } from "palette"
 import { AlbumListImage } from "./AlbumListImage"
 
 export const AlbumListItem = ({ album }: { album: Album }) => {
   const space = useSpace()
-  const itemHeight = useScreenDimensions().height / 5
   const overlapSize = space("2")
   const first3Artworks = album.artworkIds.slice(0, 3)
 
   return (
     <>
-      <Flex
-        flexDirection="row-reverse"
-        alignItems="flex-end"
-        pl={overlapSize}
-        maxHeight={itemHeight}
-        overflow="hidden"
-      >
-        {first3Artworks.length < 3 && (
-          <Flex backgroundColor="black30" flex={1} height={itemHeight} mr={-overlapSize} />
-        )}
-        {first3Artworks.length < 2 && (
-          <Flex backgroundColor="black30" flex={1} height={itemHeight - 50} mr={-overlapSize} />
-        )}
+      <Flex flexDirection="row-reverse" alignItems="flex-end" pl={overlapSize} overflow="hidden">
+        {first3Artworks.length < 3 && <Flex backgroundColor="black30" flex={1} mr={-overlapSize} />}
+        {first3Artworks.length < 2 && <Flex backgroundColor="black30" flex={1} mr={-overlapSize} />}
         {first3Artworks.reverse().map((artworkId) => (
           <AlbumListImage
             slug={artworkId}
@@ -41,6 +29,7 @@ export const AlbumListItem = ({ album }: { album: Album }) => {
           {album.artworkIds.length} Artworks
         </Text>
       </Flex>
+      <Spacer y={1} />
     </>
   )
 }
