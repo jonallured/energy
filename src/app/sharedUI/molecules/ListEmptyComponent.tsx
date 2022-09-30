ad0be882701a455555f8b1674ffb54ec2ebbcda1
Angela Patrinou <patrinoua@gmail.com>
2022-09-30 16:06:24 +0200

    size of ListEmptyComponent (#233)

--- src/app/sharedUI/molecules/ListEmptyComponent.tsx
+++ src/app/sharedUI/molecules/ListEmptyComponent.tsx
@@ -2,6 +2,8 @@ import { Flex, Text } from "palette"
 
 export const ListEmptyComponent = ({ text = "No results to display" }: { text?: string }) => (
   <Flex ml={2}>
-    <Text color="black60">{text}</Text>
+    <Text variant="xs" color="onBackgroundMedium">
+      {text}
+    </Text>
   </Flex>
 )
