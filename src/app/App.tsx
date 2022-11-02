46dfd1d8f455f31edd9fc820654b2c18fbe1483f
Pavlos Vinieratos <pvinis@gmail.com>
2022-11-02 12:06:06 +0000

    chore: ignoring some warnings until they are fixed in palette-mobile (#298)
    ignore

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,8 @@
+import { LogBox } from "react-native"
 import { AppProviders } from "./AppProviders"
 import { Main } from "./navigation/Main"
+
+LogBox.ignoreLogs(["lineHeight", "borderRadius", "fontSize", "onBackground", "borderWidth"])
 
 export const App = () => (
   <AppProviders>
