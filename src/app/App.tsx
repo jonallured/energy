e199b8a936f297c25993329f4f5008998bfa8956
Christopher Pappas <damassi.pappas@gmail.com>
2022-12-14 10:18:06 -0800

    feat(flipper): Add flipper relay tools (#367)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,9 @@
 import { LogBox } from "react-native"
 import { AppProviders } from "./AppProviders"
 import { Main } from "./navigation/Main"
+import { setupFlipper } from "./system/flipper"
+
+setupFlipper()
 
 LogBox.ignoreLogs(["lineHeight", "borderRadius", "fontSize", "onBackground", "borderWidth"])
 
