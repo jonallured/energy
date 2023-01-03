5e67d6af95aeb56425ddd34910bbc243b15c4cf2
Pavlos Vinieratos <pvinis@gmail.com>
2023-01-03 13:43:08 +0200

    fix: version/devmode toggle is back (#424)
    fix

--- src/app/screens/Settings/Settings.tsx
+++ src/app/screens/Settings/Settings.tsx
@@ -1,18 +1,8 @@
-import {
-  Spacer,
-  Button,
-  Text,
-  Separator,
-  Touchable,
-  Flex,
-  ArrowRightIcon,
-  Join,
-} from "@artsy/palette-mobile"
+import { Spacer, Button, Text, Touchable, Flex, ArrowRightIcon, Join } from "@artsy/palette-mobile"
 import { NavigationProp, useNavigation } from "@react-navigation/native"
 import { useState } from "react"
 import { getVersion } from "react-native-device-info"
 import { NavigationScreens } from "app/Navigation"
-import { DevMenu } from "app/screens/Dev/DevMenu"
 import { GlobalStore } from "app/system/store/GlobalStore"
 import { Screen } from "palette"
 
@@ -76,40 +66,41 @@ export const Settings = () => {
           </Button>
         </Join>
 
-        <Separator my={4} />
-
-        {__DEV__ && (
+        {isUserDev && (
           <Join separator={<Spacer y={1} />}>
+            <Spacer y="2" />
             <Button block onPress={() => navigation.navigate("FolioDesignLanguage")}>
               Folio Design Language
             </Button>
             <Button block onPress={() => navigation.navigate("InsteadOfStorybook")}>
               Instead Of Storybook
             </Button>
-
-            {isUserDev && <DevMenu />}
-
-            <Touchable
-              onPress={() => {
-                if (tapCount < 5) {
-                  updateTapCount(tapCount + 1)
-                } else {
-                  GlobalStore.actions.artsyPrefs.switchIsUserDev()
-                  updateTapCount(0)
-                }
-              }}
-            >
-              <Flex alignItems="center">
-                <Text variant="xs" color="onBackgroundMedium">
-                  App version: {appVersion}
-                </Text>
-                <Text variant="xs" color="onBackgroundMedium">
-                  {isUserDev && "on Developer mode"}
-                </Text>
-              </Flex>
-            </Touchable>
           </Join>
         )}
+
+        <Spacer y="2" />
+
+        <Join separator={<Spacer y={1} />}>
+          <Touchable
+            onPress={() => {
+              if (tapCount < 5) {
+                updateTapCount(tapCount + 1)
+              } else {
+                GlobalStore.actions.artsyPrefs.switchIsUserDev()
+                updateTapCount(0)
+              }
+            }}
+          >
+            <Flex alignItems="center">
+              <Text variant="xs" color="onBackgroundMedium">
+                App version: {appVersion}
+              </Text>
+              <Text variant="xs" color="onBackgroundMedium">
+                {isUserDev && "on Developer mode"}
+              </Text>
+            </Flex>
+          </Touchable>
+        </Join>
       </Screen.Body>
     </Screen>
   )
