bc609e0a7d7a94694bd17b9845a3ed56e3fb4197
Artsy <artsyit@users.noreply.github.com>
2023-03-02 13:37:37 -0500

    chore: update metaphysics graphql schema (#541)

--- src/app/system/sync/_generatedRelayChecksum.ts
+++ src/app/system/sync/_generatedRelayChecksum.ts
@@ -5,4 +5,4 @@
  * also part of the `syncManager` file, and any dependency they might have (eg. fragments).
  */
 
-export const relayChecksum = "a7e65fda167e6b05e8b9fd94e53ef9522721d65c" // pragma: allowlist secret
+export const relayChecksum = "f215a540cbadd6464c1ab76fbf114e081273e188" // pragma: allowlist secret
