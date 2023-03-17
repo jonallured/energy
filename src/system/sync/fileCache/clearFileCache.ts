cae039023920d16376028daa72a965ff536fb704
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2023-03-17 10:38:34 -0400

    fix: check for file existence before unlinking (#570)
    check for file existence before unlinking

--- src/system/sync/fileCache/clearFileCache.ts
+++ src/system/sync/fileCache/clearFileCache.ts
@@ -1,6 +1,9 @@
-import { unlink } from "react-native-fs"
+import { exists, unlink } from "react-native-fs"
 import { PATH_CACHE } from "system/sync/fileCache/constants"
 
 export const clearFileCache = async () => {
-  await unlink(PATH_CACHE)
+  const fileExists = await exists(PATH_CACHE)
+  if (fileExists) {
+    await unlink(PATH_CACHE)
+  }
 }
