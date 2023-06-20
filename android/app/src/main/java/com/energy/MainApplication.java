11c9b5b8bbc17adfbf5dc28abd7362523faecc9c
George Kartalis <g.kartalis7@gmail.com>
2023-06-20 18:10:06 +0200

    build(deps): bump react-native-reanimated to 3.2.0 from 2.14.0 (#654)
    * build(deps): bump react-native-reanimated to 3.2.0 from 2.14.0
    
    * chore: remove babel plugin, only needed for react native web
    
    * chore: refresh ci cache
    
    * refactor: clean up android native code residue
    
    * build(deps): update palette-mobile / add fastimage
    
    * feat: add ScreenDimensionsProvider from palette to support fast image hook

--- android/app/src/main/java/com/energy/MainApplication.java
+++ android/app/src/main/java/com/energy/MainApplication.java
@@ -16,7 +16,6 @@ import net.artsy.energy.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -41,11 +40,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new ReanimatedJSIModulePackage();
         }
       });
 
