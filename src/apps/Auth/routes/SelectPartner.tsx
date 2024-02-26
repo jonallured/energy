64bf6bad66c79756c1ee1039bfa5c469b4bea004
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2024-02-26 12:31:06 -0500

    fix(auth): first pass at enforcing access levels (#917)
    * first pass at enforcing access levels
    
    * update logic and clean up a bit

--- src/apps/Auth/routes/SelectPartner.tsx
+++ src/apps/Auth/routes/SelectPartner.tsx
@@ -11,7 +11,7 @@ import {
 import { SelectPartnerQuery } from "__generated__/SelectPartnerQuery.graphql"
 import { ListEmptyComponent } from "components/ListEmptyComponent"
 import { useState, useEffect, useRef, Suspense } from "react"
-import { ActivityIndicator, FlatList } from "react-native"
+import { ActivityIndicator, Alert, FlatList } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { graphql } from "react-relay"
 import { useTrackScreen } from "system/hooks/useTrackScreen"
@@ -105,6 +105,77 @@ const SelectPartner: React.FC = () => {
   const [filteredData, setFilteredData] = useState(partners)
   const insets = useSafeAreaInsets()
 
+  const showUnauthorizedAlert = ({
+    title = "You don't have access to Folio",
+    message,
+  }: {
+    title?: string
+    message: string
+  }) => {
+    Alert.alert(
+      title,
+      message,
+      [
+        {
+          text: "OK",
+          style: "cancel",
+          onPress: async () => {
+            await GlobalStore.actions.auth.signOut()
+          },
+        },
+      ],
+      { cancelable: false }
+    )
+  }
+
+  const checkPartnerAccess = async (partnerID: string) => {
+    const accessToken = GlobalStore.getState().auth.userAccessToken!
+    const result = await GlobalStore.actions.auth.gravityUnauthenticatedRequest(
+      {
+        path: `/api/v1/partner/${partnerID}/all`,
+        method: "GET",
+        headers: {
+          "X-ACCESS-TOKEN": accessToken,
+        },
+      }
+    )
+    const resJson = await result.json()
+
+    if (result.status === 200) {
+      const {
+        subscription_state,
+        has_limited_folio_access,
+        has_limited_partner_tools_access,
+      } = resJson
+
+      if (subscription_state !== "active") {
+        const message =
+          "If you think this is a mistake please reach out to your Account Executive."
+        showUnauthorizedAlert({ message })
+        return
+      }
+
+      if (!!has_limited_folio_access) {
+        const message =
+          "Talk to your Account Executive today about upgrading your plan."
+        showUnauthorizedAlert({ message })
+        return
+      }
+
+      if (!!has_limited_partner_tools_access) {
+        const message =
+          "If you think this is a mistake please reach out to your Account Executive."
+        showUnauthorizedAlert({ message })
+        return
+      }
+    } else {
+      showUnauthorizedAlert({
+        title: "Something went wrong",
+        message: "Please try again later or contact support.",
+      })
+    }
+  }
+
   useEffect(() => {
     if (!partners) return
     setFilteredData(
@@ -124,13 +195,15 @@ const SelectPartner: React.FC = () => {
           <Button
             variant="outline"
             block
-            onPress={() =>
+            onPress={async () => {
               GlobalStore.actions.auth.setActivePartnerID({
                 internalID: partner?.internalID as string,
                 name: partner?.name as string,
                 slug: partner?.slug as string,
               })
-            }
+
+              await checkPartnerAccess(partner?.internalID as string)
+            }}
           >
             {partner!.name}
           </Button>
